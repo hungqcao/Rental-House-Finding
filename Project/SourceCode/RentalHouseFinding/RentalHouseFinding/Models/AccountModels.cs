5bc3fa80d21c604af2b6feb7fb9cbdf910d6de57
vietvh01388@fpt.edu.vn <vietvh01388@fpt.edu.vn@832dac11-4e22-2d53-e69c-52df69ff2753>
2013-03-09 09:14:56 +0000

    fblogin - vietvh

--- Project/SourceCode/RentalHouseFinding/RentalHouseFinding/Models/AccountModels.cs
+++ Project/SourceCode/RentalHouseFinding/RentalHouseFinding/Models/AccountModels.cs
@@ -100,10 +100,16 @@ namespace RentalHouseFinding.Models
 
     public class UserDetailsModel
     {
-        public string OpenID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Gender { get; set; }
-        public string Email { get; set; }
+        public string id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string gender { get; set; }
+        public string email { get; set; }        
+        public string locale { get; set; }
+        public string link { get; set; }
+        public string username { get; set; }
+        public int timezone { get; set; }
+        //public FacebookLocation location { get; set; }
+        public Picture picture { get; set; }
     }
 }
